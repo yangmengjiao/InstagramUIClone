da67ae5010f31b4d97b27bda4427deee58d0f88e
yangmengjiao <mengjiaoyang7@gmail.com>
2023-01-01 11:25:04 -0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by mengjiao yang on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
